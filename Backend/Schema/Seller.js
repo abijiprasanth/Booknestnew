b7811fe5629344f0586a3c98feee5eef3895214f
VARDHAN REDDY <149311037+VardhanReddy06@users.noreply.github.com>
2024-07-20 16:03:58 +0530

    Delete Backend directory

--- Backend/Schema/Seller.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-const Schema = mongoose.Schema;
-const SellerSchema = new Schema({
-    name:{type:String , required:true},
-    password:{type:String , required:true},
-    email:{type:String , required:true},
-})
-const Seller = mongoose.model('Seller', SellerSchema);
-module.exports = Seller;
